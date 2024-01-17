a34fd30cff294312c2de3434d7e664a95c718f72
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-01-16 23:26:42 -0500

    Add files via upload

---
+++ HackerRank/Loops.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Given an integer, n, 
+# ## For all non-negative integers, i < n,
+# ## Print i squared (i^2)
+
+# In[1]:
+
+
+# Initialize n
+n = 3
+
+
+# In[3]:
+
+
+# Create a loop that prints the square of each integer less than n
+for i in range(n):
+    print(i*i)
+
+
+# In[ ]:
+
+
+
+
