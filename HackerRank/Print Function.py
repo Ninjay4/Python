8a1f60537958170efab07a0a8ddaf49ae7cb1145
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-01-18 23:04:51 -0500

    Add files via upload

---
+++ HackerRank/Print Function.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Given an integer, n, print 1234...n
+# Do NOT use any string methods
+
+
+# In[2]:
+
+
+n = 16 # The given integer
+
+
+# In[3]:
+
+
+# Create a loop that prints all integers
+# from 1 to n, in a single line with no spaces
+for i in range(1, n+1):
+    print(i, end='') # There is no space between the apostrophes
+
+
+# In[ ]:
+
+
+
+
