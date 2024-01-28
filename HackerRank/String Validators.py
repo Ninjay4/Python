3d3075b989e3e86aeeab79f1f135861019c7f787
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-01-27 20:58:09 -0500

    Add files via upload

---
+++ HackerRank/String Validators.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Use built-in string validator methods
+# To check if a string is composed of 
+# Any Alphabetical, Alphanumeric, Upper,
+# or Lower characters 
+
+
+# In[2]:
+
+
+# Given a string, check it to see if
+# It contains any of the characters named above
+
+
+# In[3]:
+
+
+# Create the input string as a single line
+# Enter "qA2" as the string
+s = input("Enter the string: ")
+
+
+# In[4]:
+
+
+# Must have 5 lines of output
+# In the first line, print True if "the String" has any alphanumeric characters. Otherwise, print False.
+# In the second line, print True if "the String" has any alphabetical characters. Otherwise, print False.
+# In the third line, print True if "the String" has any digits. Otherwise, print False.
+# In the fourth line, print True if "the String" has any lowercase characters. Otherwise, print False.
+# In the fifth line, print True if "the String" has any uppercase characters. Otherwise, print False.
+
+
+# In[6]:
+
+
+# Create the code as a series of Conditional statements
+# Use the any() Function
+if any(e.isalnum() for e in s): # Alphanumeric is alnum
+    print('True')
+else:
+    print('False')
+if any(e.isalpha() for e in s): # Alphabetical is alpha
+    print('True')
+else:
+    print('False')
+if any(e.isdigit() for e in s): # Digits are numerical digits
+    print('True')
+else:
+    print('False')
+if any(e.islower() for e in s): # Lowercase is lower
+    print('True')
+else:
+    print('False')
+if any(e.isupper() for e in s): # Uppercase is upper
+    print('True')
+else:
+    print('False')
+
+
+# In[ ]:
+
+
+
+
