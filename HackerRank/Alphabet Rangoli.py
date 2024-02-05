9a83ac2fdc4e9c4df8527c6ef0274866fdef0382
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-04 23:20:19 -0500

    Update Alphabet Rangoli.py
    Added a Header and cleaned up code

--- HackerRank/Alphabet Rangoli.py
+++ HackerRank/Alphabet Rangoli.py
@@ -1,42 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-# Given an integer n, from 1 to 26, print an alphabet rangoli of size n
-# Only use lowercase letters and hyphens
-
-
-# In[2]:
-
+‘’’
+Created by Ninjay4
+	On 1/26/24
+	File Name: Alphabet Rangoli.py
+	Description:  HackerRank Basic Python Challenge 
+        Given an integer "n", from 1 to 26, print an alphabet rangoli of size "n"
+        Only use lowercase letters and hyphens
+	Python Version: 3
+‘’’
 
 # Create an alphabet dictionary
 alpha = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:'j', 11:'k', 12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u', 22:'v', 23:'w', 24:'x', 25:'y', 26:'z'}
 
-
-# In[3]:
-
-
 print(alpha[6]) # Test
-
-
-# In[4]:
-
 
 # Create the input integer
 n = int(input("Enter an integer from 1 to 26: "))
 
-
-# In[5]:
-
-
 # Create the print_rangoli Function
-
-
-# In[6]:
-
-
 def print_rangoli(n):
 # Create a loop for the top half
     front = ""
@@ -55,13 +37,6 @@ def print_rangoli(n):
         middle2 = middle2.replace(alpha[i] + "-" + alpha[i-1] +"-"+alpha[i], alpha[i])
         print(dashes + middle2 + dashes)
     
-   
-   
-
-
-# In[7]:
-
-
 # Run the print_rangoli Function
 print_rangoli(n)
 
