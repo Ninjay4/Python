2ae80d7d7bb3704fc510a69513317b84ca7c1779
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-06 23:04:20 -0500

    Update Write A Function.py
    Added a Header section and cleaned up the code

--- HackerRank/Write A Function.py
+++ HackerRank/Write A Function.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-#  Determine if a given year is a leap year
-#  In the Gregorian calendar, three conditions are used to identify leap years:
-#  The year can be evenly divided by 4, is a leap year, unless:
-#  The year can be evenly divided by 100, it is NOT a leap year, unless:
-#  The year is also evenly divisible by 400. Then it is a leap year.
-
-
-# In[2]:
-
+‘’’
+Created by Ninjay4
+	On 1/20/24
+	File Name: Write A Function.py
+    HackerRank Basic Python Challenge
+	Description: Determine if a given year is a leap year
+      In the Gregorian calendar, three conditions are used to identify leap years:
+      The year can be evenly divided by 4, is a leap year, unless:
+      The year can be evenly divided by 100, it is NOT a leap year, unless:
+      The year is also evenly divisible by 400. Then it is a leap year.
+	Python Version: 3
+‘’’
 
 #  Given a year, determine whether it is a leap year.    
 #  If it is a leap year, return the Boolean True, otherwise return False.
-
-
-# In[6]:
-
 
 def is_leap(year):  # Create the "is_leap" function
     leap = False  # Make False the default value
@@ -27,21 +22,11 @@ def is_leap(year):  # Create the "is_leap" function
         leap = True  # If the conditions are met, then the value is True for a Leap Year
     return leap # The return value closes the function
 
-
-# In[11]:
-
-
+# Create the input value
 year = int(input("Please enter the year: "))
 
-
-# In[12]:
-
-
+# Run the is_leap() Function
 is_leap(year)
-
-
-# In[ ]:
-
 
 
 
