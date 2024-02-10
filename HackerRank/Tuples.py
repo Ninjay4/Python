895da883b66920700072d07e0a44acb359715df0
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-09 23:27:18 -0500

    Update Tuples.py
    Created a header

--- HackerRank/Tuples.py
+++ HackerRank/Tuples.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
-# Given an integer,"n", and "n" space-separated integers as input, 
-# create a tuple,"t", of those "n" integers
-# Then compute and print the result of hash(t) 
+‘’’
+Created by Ninjay4
+	On 1/26/24
+	File Name: Tuples.py
+  HackerRank Basic Python Challenge
+	Description: Given an integer,"n", and "n" space-separated integers as input, 
+    create a tuple,"t", of those "n" integers
+    Then compute and print the result of hash(t) 
+	Python Version: 3
+‘’’
 
 n = int(input()) # Enter "2"
 
