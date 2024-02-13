61577bd248536b983372441ce13d7ae0d0a024a5
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-12 23:21:16 -0500

    Update Text Wrap.py
    Added a header and cleaned-up the code

--- HackerRank/Text Wrap.py
+++ HackerRank/Text Wrap.py
@@ -1,41 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-# Given a string and a maximun width, "max_width"
-# Wrap the string into a paragraph of where
-# Each line does NOT exceed the maximum width
-
-
-# In[2]:
-
-
-# wrap has the following parameters:
-# string: a long string
-# int max_width: the width to wrap to
-# Constraints:
-# 0 < string length < 1,000
-# 0 < max_width < string length
-
-
-# In[3]:
-
-
-# Returns a string: a single string with newline characters ('\n') 
-# where the breaks should be
-
-
-# In[4]:
-
+‘’’
+Created by Ninjay4
+	On 2/10/24
+	File Name: Text Wrap.py
+    HackerRank Basic Python Challenge
+	Description: Given a string and a maximun width, "max_width"
+        Wrap the string into a paragraph of where
+        Each line does NOT exceed the maximum width
+        The wrap has the following parameters:
+            string: a long string
+            int max_width: the width to wrap to
+        Constraints:
+            0 < string length < 1,000
+            0 < max_width < string length
+        Returns a string: a single string with newline characters ('\n') 
+            where the breaks should be
+	Python Version: 3
+‘’’
 
 # import the textwrap function
 import textwrap
-
-
-# In[5]:
-
 
 # Create a wrap function
 # The "wrap" function returns a list
@@ -46,10 +31,6 @@ def wrap(string, max_width):
     twstring = textwrap.wrap(string, max_width)
     return '\n'.join(twstring)
 
-
-# In[6]:
-
-
 # Input Format
 # The first line contains the string
 # The second line contains the max_width
@@ -59,16 +40,8 @@ def wrap(string, max_width):
 string = input("Enter the String: ")
 max_width = int(input("Enter the maximum width of each new line: "))
 
-
-# In[7]:
-
-
 # Call the wrap function as a result
 result = wrap(string, max_width)
-
-
-# In[8]:
-
 
 # Print the result
 print(result)
