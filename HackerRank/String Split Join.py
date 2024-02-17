01209bc6981fc18f4a1787e2910249fda0e732ce
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-16 23:14:41 -0500

    Update String Split Join.py
    Created a header and cleaned-up code

--- HackerRank/String Split Join.py
+++ HackerRank/String Split Join.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-# Given a string, split it on a " " (space) delimiter and join using a - hyphen
-
-
-# In[2]:
-
+‘’’
+Created by Ninjay4
+	On 1/22/24
+	File Name: String Split Join.py
+    HackerRank Basic Python Challenge
+	Description: Given a string, split it on a " " (space) delimiter and join using a - hyphen
+	Python Version: 3
+‘’’
 
 # Create a "Split and Join" Function
-
-
-# In[3]:
-
 
 def split_and_join(line):
     # sl = line.split(" ") # Splits the given line into a list with elements separated by a space " "
@@ -23,30 +18,15 @@ def split_and_join(line):
     sj = "-".join(line.split(" ")) # The short and most efficient way that combines the steps above into one statement
     return sj
 
-
-# In[4]:
-
-
 # The one line contains a string consisting of space separated words
 # Enter the given line "this is a string"
 line = (input("Enter the string: "))
 
-
-# In[5]:
-
-
 # Call the "split and join" function as a "result"
 result = split_and_join(line)
 
-
-# In[6]:
-
-
 # Print the result
 print(result)
-
-
-# In[ ]:
 
 
 
