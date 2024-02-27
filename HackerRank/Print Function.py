2e17a42a9f672029dcf69574f4c3d52c2f3de1b5
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-26 23:44:53 -0500

    Update Print Function.py
    Created a header and cleaned-up code

--- HackerRank/Print Function.py
+++ HackerRank/Print Function.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-# Given an integer, n, print 1234...n
-# Do NOT use any string methods
-
-
-# In[2]:
-
+'''
+File Name: Print Function
+Created By: Ninjay4
+Created On: 1/18/24
+Modified On: None
+HackerRank Basic Python Challenge
+Description:  Given an integer, n, print 1234...n
+Do NOT use any string methods
+'''
 
 n = 16 # The given integer
-
-
-# In[3]:
-
 
 # Create a loop that prints all integers
 # from 1 to n, in a single line with no spaces
 for i in range(1, n+1):
     print(i, end='') # There is no space between the apostrophes
-
-
-# In[ ]:
-
 
 
 
