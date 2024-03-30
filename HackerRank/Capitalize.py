546a9fd81210bb98473c94dee96bd0ab12e35075
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-29 23:37:22 -0400

    Update Capitalize.py
    Created a header section

--- HackerRank/Capitalize.py
+++ HackerRank/Capitalize.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-# Given a first and last name, capitalize the 1st letters of each word
-# A single line of input contains the full name
-
-
-# In[2]:
-
-
+'''
+FILE NAME: Capitalize
+CREATED BY: Ninjay4
+CREATED ON:
+MODIFIED ON: none
+DESCRIPTION:
+Given a first and last name, capitalize the 1st letters of each word
+A single line of input contains the full name
+'''
 # Create a capitalization function
 # This function only works when a name section does NOT have
 # any apostrophe's nor hyphens
@@ -18,34 +16,20 @@
 #    caps = s.title()
 #    return caps
 
-
-# In[5]:
-
-
 # Create another capitalization function
 import string
 def solve(s):
     caps = string.capwords(s, " ")
     return caps
 
-
-# In[8]:
-
-
 # Create the input string
 # Enter "chris alan" in lowercase letters
 s = input("Enter the first and last name: ")
-
-
-# In[9]:
-
 
 # Run the function
 result = solve(s)
 print(result)
 
-
-# In[ ]:
 
 
 
